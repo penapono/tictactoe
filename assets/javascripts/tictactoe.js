0a88552244c377b4e4730f96757ac6c284d7a803
dbernucio” <dbernucio@caiena.net>
2016-04-08 14:54:35 -0300

    Refatorando o código

--- assets/javascripts/tictactoe.js
+++ assets/javascripts/tictactoe.js
@@ -1,35 +1,38 @@
 $(function() {
 
   // Variáveis globais: podem ser usadas em todo o documento
-  var turno_x = true,
-      ganhador = '',
-      cedulas = $('.cedula');
+  var campos = $('td'),
+      jogador = 'x';
 
   // Evento de clique em uma das cédulas do tabuleiro
-  cedulas.on('click', function() {
-    if (!($(this).hasClass('x')) && (!$(this).hasClass('o'))) {
-      if (turno_x) {
-        $(this).addClass('x');
-        $(this).val('X');
-      }
-      else {
-        $(this).addClass('o');
-        $(this).val('O');
-      }
+  campos.on('click', function() {
+    var campo_clicado = $(this);
 
-      // Muda o turno_x para a próxima jogada
-      // turno_x == true -> jogador X
-      // turno_x == false -> jogador O
-      turno_x = !turno_x;
-
+    if (campo_clicado.attr('valor') === '') {
+      marcaCampo(campo_clicado);
       verificaFimJogo();
+      proximoJogador();
     }
   });
 
+  function marcaCampo(campo_clicado) {
+    campo_clicado.addClass(jogador);
+    campo_clicado.attr('valor', jogador);
+  }
+
+  function proximoJogador() {
+    if (jogador === 'x') {
+      jogador = 'o';
+    }
+    else {
+      jogador = 'x';
+    }
+  }
+
   // Função que verifica se o jogo já terminou
   function verificaFimJogo() {
-    if(verificaHorizontal() || verificaVertical() || verificaDiagonal()) {
-      alert('O jogador ' + ganhador + ' ganhou!');
+    if(acertouHorizontal() || acertouVertical() || acertouDiagonal()) {
+      alert('O jogador ' + jogador.toUpperCase() + ' ganhou!');
       // Limpa o tabuleiro e começa novo jogo
       limpaTabuleiro();
     }
@@ -43,111 +46,100 @@ $(function() {
   }
 
   // Verifica se as linhas da tabela possuem símbolos iguais
-  function verificaHorizontal() {
-    // Linha 1 -> cédula 0, cédula 1 e cédula 2
-    if (!cedulasVazias($('#0'), $('#1'), $('#2'))) {
-      if ($('#0').val() === $('#1').val() && $('#1').val() === $('#2').val()) {
-        ganhador = $('#0').val();
-        return true;
-      }
+  function acertouHorizontal() {
+    // Linha 1 -> campos 1, 2 e 3
+    if (!camposVazios(1,2,3) && camposIguais(1,2,3)) {
+      return true;
     }
 
-    // Linha 2 -> cédula 3, cédula 4 e cédula 5
-    if (!cedulasVazias($('#3'), $('#4'), $('#5'))) {
-      if ($('#3').val() === $('#4').val() && $('#4').val() === $('#5').val()) {
-        ganhador = $('#3').val();
-        return true;
-      }
+    // Linha 2 -> campos 4, 5 e 6
+    if (!camposVazios(4,5,6) && camposIguais(4,5,6)) {
+      return true;
     }
 
-    // Linha 3 -> cédula 6, cédula 7 e cédula 8
-    if (!cedulasVazias($('#6'), $('#7'), $('#8'))) {
-      if ($('#6').val() === $('#7').val() && $('#7').val() === $('#8').val()) {
-        ganhador = $('#6').val();
-        return true;
-      }
+    // Linha 3 -> campos 7, 8 e 9
+    if (!camposVazios(7,8,9) && camposIguais(7,8,9)) {
+      return true;
     }
 
     return false;
   }
 
   // Verifica se as colunas da tabela possuem símbolos iguais
-  function verificaVertical() {
-    // Coluna 1 -> cédula 0, cédula 3 e cédula 6
-    if (!cedulasVazias($('#0'), $('#3'), $('#6'))) {
-      if ($('#0').val() === $('#3').val() && $('#3').val() === $('#6').val()) {
-        ganhador = $('#0').val();
-        return true;
-      }
+  function acertouVertical() {
+    // Coluna 1 -> campos 1, 4 e 7
+    if (!camposVazios(1,4,7) && camposIguais(1,4,7)) {
+      return true;
     }
 
-    // Coluna 2 -> cédula 1, cédula 4 e cédula 7
-    if (!cedulasVazias($('#1'), $('#4'), ('#7'))) {
-      if ($('#1').val() === $('#4').val() && $('#4').val() === $('#7').val()) {
-        ganhador = $('#1').val();
-        return true;
-      }
+    // Coluna 2 -> campos 2, 5 e 8
+    if (!camposVazios(2,5,8) && camposIguais(2,5,8)) {
+      return true;
     }
 
-    // Coluna 3 -> cédula 2, cédula 5 e cédula 8
-    if (!cedulasVazias($('#2'), $('#5'), $('#8'))) {
-      if ($('#2').val() === $('#5').val() && $('#5').val() === $('#8').val()) {
-        ganhador = $('#2').val();
-        return true;
-      }
+    // Coluna 3 -> campos 3, 6 e 9
+    if (!camposVazios(3,6,9) && camposIguais(3,6,9)) {
+      return true;
     }
 
     return false;
   }
 
   // Verifica se as diagonais da tabela possuem símbolos iguais
-  function verificaDiagonal() {
-    // Diagonal -> cédula 0, cédula 4 e cédula 8
-    if (!cedulasVazias($('#0'), $('#4'), $('#8'))) {
-      if ($('#0').val() === $('#4').val() && $('#4').val() === $('#8').val()) {
-        ganhador = $('#0').val();
-        return true;
-      }
+  function acertouDiagonal() {
+    // Diagonal -> campos 1, 5 e 9
+    if (!camposVazios(1,5,9) && camposIguais(1,5,9)) {
+      return true;
     }
 
-    // Diagonal -> cédula 2, cédula 4 e cédula 6
-    if (!cedulasVazias($('#2'), $('#4'), $('#6'))) {
-      if ($('#2').val() === $('#4').val() && $('#4').val() === $('#6').val()) {
-        ganhador = $('#2').val();
-        return true;
-      }
+    // Diagonal -> campos 3, 5 e 7
+    if (!camposVazios(3,5,7) && camposIguais(3,5,7)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function camposIguais(parametro1, parametro2, parametro3) {
+    var valor1 = $('#' + parametro1);
+    var valor2 = $('#' + parametro2);
+    var valor3 = $('#' + parametro3);
+
+    if(valor1.attr('valor') === valor2.attr('valor') &&
+        valor2.attr('valor') == valor3.attr('valor')) {
+      return true;
     }
     return false;
   }
 
-  // Verifica se todas as cédulas estão preenchidas
+  function camposVazios(parametro1, parametro2, parametro3) {
+    var valor1 = $('#' + parametro1);
+    var valor2 = $('#' + parametro2);
+    var valor3 = $('#' + parametro3);
+
+    if((valor1.attr('valor') === '' || valor2.attr('valor') === '' ||
+      valor3.attr('valor') === '')) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // Verifica se todas as campos estão preenchidas
   function verificaVelha() {
-    for(var i = 0; i < cedulas.length; i++) {
-      if($(cedulas[i]).val() === "") {
+    for(var i = 0; i < campos.length; i++) {
+      if($(campos[i]).attr('valor') === '') {
         return false;
       }
     }
     return true;
   }
 
-  // Uma cédula está vazia quando ela não possui nem a classe ".o" nem a ".x"
-  function cedulasVazias(cedula1, cedula2, cedula3) {
-    if((cedula1.val() === "") && (cedula2.val() === "") && (cedula3.val() === "")) {
-      return true;
-    }
-    return false;
-  }
-
-  // Limpa tanto a imagem, quanto o valor da cédula
+  // Limpa tanto a imagem, quanto o valor da campo
   function limpaTabuleiro() {
-    for(var i = 0; i < cedulas.length; i++) {
-      if($(cedulas[i]).hasClass('o')) {
-        $(cedulas[i]).removeClass('o');
-      }
-      else if($(cedulas[i]).hasClass('x')) {
-        $(cedulas[i]).removeClass('x');
-      }
-      $(cedulas[i]).val('');
+    for(var i = 0; i < campos.length; i++) {
+      $(campos[i]).attr('valor', '');
+      $(campos[i]).removeClass();
     }
   }
 });
